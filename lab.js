b6c097fca583a07a9bfc486b3cc0fff083888481
roseperuski <roseperuskigithub@gmail.com>
2020-12-17 22:01:09 -0500

    mountain portion

---
+++ lab.js
@@ -0,0 +1,34 @@
+"use strict";
+const mountains = [
+    { name: "Kiliminjaro", height: 19341 },
+    {
+        name: "Everest",
+        height: 29029,
+    },
+    {
+        name: "Denali",
+        height: 20310,
+    }
+];
+//Declare a function called findNameOfTallestMountain. It takes one parameter, an
+// array of Mountain objects. It returns a string, the name of the tallest mountain in the
+// given array. If the array argument is empty, return an empty string ("").
+// findName
+function findNameOfTallestMountain(mtns) {
+    if (mtns === null) {
+        return "";
+    }
+    else {
+        let height = 0;
+        let tallest = "";
+        mtns.forEach(element => {
+            if (element.height > height) {
+                height = element.height;
+                tallest = element.name;
+            }
+        });
+        return tallest;
+    }
+}
+const tallestMtn = findNameOfTallestMountain(mountains);
+console.log(tallestMtn);
--- lab.ts
+++ lab.ts
@@ -1,6 +1,42 @@
 interface Mountain {
-    name: string;
-    height: number;
+  name: string;
+  height: number;
 }
 
-const mountains : Mountain[]= [];
+const mountains: Mountain[] = [
+  { name: "Kiliminjaro", height: 19341 },
+
+  {
+    name: "Everest",
+    height: 29029,
+  },
+
+  {
+      name: "Denali",
+      height: 20310,
+  }
+];
+
+//Declare a function called findNameOfTallestMountain. It takes one parameter, an
+// array of Mountain objects. It returns a string, the name of the tallest mountain in the
+// given array. If the array argument is empty, return an empty string ("").
+// findName
+function findNameOfTallestMountain(mtns: Mountain[]) : string {
+    if (mtns === null){
+        return "";
+    } else {
+        let height = 0;
+        let tallest="";
+        mtns.forEach(element => {
+            if (element.height > height){
+                height=element.height;
+                tallest=element.name;
+            }
+        });
+        return tallest;
+    }
+    
+}
+
+const tallestMtn = findNameOfTallestMountain(mountains);
+console.log(tallestMtn);
